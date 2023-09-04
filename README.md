3c11fd2ee78b34695bc7c554241bfa1fb8fa86c3
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-09-03 21:15:41 -0300

    task #1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Running
+
+Upload "MAIN COLAB.ipynb" to Google Colab and run it. It will download the dataset and the models and run the models on the dataset.
+
+If running locally, download the dataset, unzip it in the root of the repository and download the models and place them in the `models` folder, then run the MAIN.ipynb notebook.
+
+
+
+# Links
+
+- [Dataset](https://www.dropbox.com/s/ut350iwgby9swk2/Sketch_EITZ.zip)
+- [Simple Model](https://raw.githubusercontent.com/jmsaavedrar/machine_learning/6c27f6972f17af0542cfa028857a1d37225053a6/convnet/simple.py)
+- [ResNet-18 Model](https://raw.githubusercontent.com/jmsaavedrar/machine_learning/315e175a2d88bca7aa9177a516183cbaf96a46c8/convnet/resnet.py)
---
+++ metrics.py
@@ -0,0 +1,48 @@
+# coefficient of determination
+import numpy as np
+
+
+def r2score(y_true, y_pred):
+    mu = np.mean(y_true)
+    sigma = np.sum(np.square(y_true - mu))
+    sigma_r = np.sum(np.square(y_true - y_pred))
+    return 1 - sigma_r / sigma
+
+
+def accuracy(y_true, y_pred):
+    y_pred = y_pred.copy()
+    y_pred[y_pred < 0.5] = 0
+    y_pred[y_pred >= 0.5] = 1
+    acc = np.mean(np.equal(y_true, y_pred).astype(dtype=np.int32))
+    return acc
+
+
+def multiclass_accuracy(y_true, y_pred):
+    y_pred = y_pred.copy()
+    y_pred = np.argmax(y_pred, axis=1, keepdims=False)
+    acc = np.mean(
+        np.equal(np.squeeze(y_true), np.squeeze(y_pred)).astype(dtype=np.int32)
+    )
+    return acc
+
+
+# def confusion_matrix(y_true, y_pred, n_classes):
+#     y_pred = np.argmax(y_pred, axis=1, keepdims=True)
+#     cm = np.zeros((n_classes, n_classes), dtype=np.int32)
+#     for cl_true in np.arange(n_classes):
+#         y = y_pred[y_true == cl_true]
+#         for cl_pred in np.arange(n_classes):
+#             cm[cl_true, cl_pred] = np.sum(y == cl_pred)
+#     return cm
+
+def confusion_matrix(y_true, y_pred, n_classes):
+    # Convert one-hot encoding to class labels
+    y_true_labels = np.argmax(y_true, axis=1)
+    y_pred_labels = np.argmax(y_pred, axis=1)
+
+    cm = np.zeros((n_classes, n_classes), dtype=np.int32)
+    for cl_true in np.arange(n_classes):
+        y = y_pred_labels[y_true_labels == cl_true]
+        for cl_pred in np.arange(n_classes):
+            cm[cl_true, cl_pred] = np.sum(y == cl_pred)
+    return cm
